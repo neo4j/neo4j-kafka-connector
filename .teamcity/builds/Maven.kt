5812f24c7671c104282eeaa3b0690ee744082016
Ali Ince <ali.ince@neotechnology.com>
2023-07-17 12:13:04 +0100

    ci: use build scoped maven repository

--- .teamcity/builds/Maven.kt
+++ .teamcity/builds/Maven.kt
@@ -3,6 +3,7 @@ package builds
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.buildSteps.MavenBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.MavenBuildStep.RepositoryScope
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.toId
 
@@ -12,20 +13,14 @@ class Maven(id: String, name: String, goals: String, args: String? = null) : Bui
 
     steps {
         maven {
-            this.goals = "install"
-            this.pomLocation = "build-resources/pom.xml"
-
-            dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
-            dockerImage = "eclipse-temurin:11-jdk"
-            dockerRunParameters = "--volume /var/run/docker.sock:/var/run/docker.sock"
-        }
-        maven {
             this.goals = goals
             this.runnerArgs = args
 
             dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
             dockerImage = "eclipse-temurin:11-jdk"
             dockerRunParameters = "--volume /var/run/docker.sock:/var/run/docker.sock"
+
+            localRepoScope = RepositoryScope.BUILD_CONFIGURATION
         }
     }
 
