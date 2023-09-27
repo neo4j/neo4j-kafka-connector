a654010df0f141a472b584ce499b3d56ffc95528
Ali Ince <ali-ince@users.noreply.github.com>
2023-09-27 09:43:51 +0100

    build: split projects (#10)

--- .teamcity/builds/Maven.kt
+++ .teamcity/builds/Maven.kt
@@ -6,35 +6,49 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.MavenBuildStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.toId
 
-class Maven(id: String, name: String, goals: String, args: String? = null) : BuildType({
-    this.id(id.toId())
-    this.name = name
+class Maven(id: String, name: String, goals: String, args: String? = null) :
+    BuildType({
+      this.id(id.toId())
+      this.name = name
 
-    steps {
+      // we uploaded a custom settings.xml file in Teamcity UI, under Connectors project
+      // with the following content, so we set the relevant environment variables here.
+
+      /*
+      <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                                    http://maven.apache.org/xsd/settings-1.0.0.xsd">
+          <servers>
+            <server>
+              <id>github</id>
+              <username>${env.PACKAGES_USERNAME}</username>
+              <password>${env.PACKAGES_PASSWORD}</password>
+            </server>
+          </servers>
+      </settings>
+       */
+
+      params {
+        text("env.PACKAGES_USERNAME", "%github-packages-user%")
+        password("env.PACKAGES_PASSWORD", "%github-packages-token%")
+      }
+
+      steps {
         maven {
-            this.goals = "install"
-            this.pomLocation = "build-resources/pom.xml"
+          this.goals = goals
+          this.runnerArgs = args
 
-            dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
-            dockerImage = "eclipse-temurin:11-jdk"
-            dockerRunParameters = "--volume /var/run/docker.sock:/var/run/docker.sock"
+          // this is the settings name we uploaded to Connectors project
+          userSettingsSelection = "github"
+
+          dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
+          dockerImage = "eclipse-temurin:11-jdk"
+          dockerRunParameters = "--volume /var/run/docker.sock:/var/run/docker.sock"
         }
-        maven {
-            this.goals = goals
-            this.runnerArgs = args
+      }
 
-            dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
-            dockerImage = "eclipse-temurin:11-jdk"
-            dockerRunParameters = "--volume /var/run/docker.sock:/var/run/docker.sock"
-        }
-    }
+      features { dockerSupport {} }
 
-    features {
-        dockerSupport {
-        }
-    }
-
-    requirements {
-        runOnLinux(LinuxSize.SMALL)
-    }
-})
+      requirements { runOnLinux(LinuxSize.SMALL) }
+    })
