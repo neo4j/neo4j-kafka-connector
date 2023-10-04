461b78c8ff45b6719a4627571cebdb065fec2909
Florent Biville <florent.biville@neo4j.com>
2023-10-04 16:20:21 +0200

    feat: introduce basic version of end-to-end test

---
+++ source/src/test/kotlin/org/neo4j/connectors/kafka/source/Neo4jSourceIT.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package streams.kafka.connect.source
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import java.time.Duration
+import java.util.*
+import java.util.concurrent.TimeUnit
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.awaitility.Awaitility.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.Driver
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.Session
+import streams.kafka.connect.source.testing.Neo4jSourceRegistration
+
+class Neo4jSourceIT {
+
+  companion object {
+    private lateinit var sourceRegistration: Neo4jSourceRegistration
+    private lateinit var driver: Driver
+    private lateinit var session: Session
+
+    @BeforeAll
+    @JvmStatic
+    fun setUpConnector() {
+      driver = GraphDatabase.driver("neo4j://localhost", AuthTokens.basic("neo4j", "password"))
+      session = driver.session()
+      sourceRegistration =
+          Neo4jSourceRegistration(
+              topic = "test-topic",
+              neo4jServerUri = "neo4j://neo4j",
+              neo4jBasicPassword = "password",
+              streamingNeo4jProperty = "timestamp",
+              streamingStart = StreamingFrom.ALL,
+              streamingQuery =
+                  "MATCH (ts:TestSource) WHERE ts.timestamp > \$lastCheck RETURN ts.name AS name, ts.surname AS surname, ts.timestamp AS timestamp, ts.execId AS execId",
+              schemaControlRegistry = "http://schema-registry:8081",
+          )
+      sourceRegistration.register("http://localhost:8083")
+    }
+
+    @AfterAll
+    @JvmStatic
+    fun tearDownConnector() {
+      session.close()
+      driver.close()
+      sourceRegistration.unregister()
+    }
+  }
+
+  @Test
+  fun `reads latest changes from Neo4j source`(testInfo: TestInfo) {
+    val executionId = testInfo.displayName + System.currentTimeMillis()
+    val consumer = subscribeConsumerTo(testInfo, "test-topic", "latest")
+
+    session
+        .run(
+            "CREATE (:TestSource {name: 'jane', surname: 'doe', timestamp: datetime().epochMillis, execId: \$execId})",
+            mapOf("execId" to executionId))
+        .consume()
+    session
+        .run(
+            "CREATE (:TestSource {name: 'john', surname: 'doe', timestamp: datetime().epochMillis, execId: \$execId})",
+            mapOf("execId" to executionId))
+        .consume()
+    session
+        .run(
+            "CREATE (:TestSource {name: 'mary', surname: 'doe', timestamp: datetime().epochMillis, execId: \$execId})",
+            mapOf("execId" to executionId))
+        .consume()
+
+    val expected =
+        setOf(
+            mapOf("name" to "jane", "surname" to "doe", "execId" to executionId),
+            mapOf("name" to "john", "surname" to "doe", "execId" to executionId),
+            mapOf("name" to "mary", "surname" to "doe", "execId" to executionId))
+    val allRecords: MutableList<Map<String, String>> =
+        mutableListOf() // todo: impl ringbuffer instead?
+    await().atMost(60, TimeUnit.SECONDS).until {
+      consumer.poll(Duration.ofSeconds(10)).forEach { allRecords.add(it.value().asMap()) }
+      val records = allRecords.takeLast(expected.size).reversed()
+      records.isNotEmpty() &&
+          records.fold(true) { prev, map ->
+            prev &&
+                map.containsKey("timestamp") &&
+                expected.contains(map.filterKeys { it != "timestamp" })
+          }
+    }
+  }
+
+  private fun subscribeConsumerTo(
+      testInfo: TestInfo,
+      topic: String,
+      offset: String
+  ): KafkaConsumer<String, GenericRecord> {
+    val properties = Properties()
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    properties.setProperty(
+        KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081")
+    properties.setProperty(
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+        StringDeserializer::class.java.getName(),
+    )
+    properties.setProperty(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+        KafkaAvroDeserializer::class.java.getName(),
+    )
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, testInfo.displayName)
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset)
+    val consumer = KafkaConsumer<String, GenericRecord>(properties)
+    consumer.subscribe(listOf(topic))
+    return consumer
+  }
+}
+
+fun GenericRecord.asMap(): Map<String, String> {
+  return this.schema.fields
+      // FIXME: properly convert values
+      .map { field -> field.name() to this.get(field.name()).toString() }
+      .toMap()
+}
---
+++ source/src/test/kotlin/org/neo4j/connectors/kafka/source/testing/Neo4jSourceRegistration.kt
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package streams.kafka.connect.source.testing
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublishers
+import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.streams.asSequence
+import streams.kafka.connect.source.StreamingFrom
+
+data class Neo4jSourceRegistration(
+    private val topic: String,
+    private val neo4jServerUri: String,
+    private val neo4jBasicUsername: String = "neo4j",
+    private val neo4jBasicPassword: String,
+    private val pollInterval: Duration = Duration.ofMillis(5000),
+    private val enforceSchema: Boolean = true,
+    private val streamingNeo4jProperty: String,
+    private val streamingStart: StreamingFrom,
+    private val streamingQuery: String,
+    private val schemaControlRegistry: String
+) {
+
+  private val name: String = randomized("Neo4jSourceConnector")
+  private lateinit var controlCenterBaseUri: String
+
+  fun register(controlCenterUri: String) {
+    controlCenterBaseUri = controlCenterUri
+    val uri = URI("$controlCenterBaseUri/connectors")
+    val requestBody = registrationJson()
+    val registration =
+        HttpRequest.newBuilder(uri)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .POST(BodyPublishers.ofString(requestBody))
+            .build()
+    val response = HttpClient.newHttpClient().send(registration, BodyHandlers.ofString())
+    if (response.statusCode() != 201) {
+      val error =
+          String.format(
+              "Could not register source, expected 201, got: %s\n%s",
+              response.statusCode(),
+              response.body(),
+          )
+      throw RuntimeException(error)
+    }
+  }
+
+  fun unregister() {
+    val uri = URI("$controlCenterBaseUri/connectors/$name/")
+    val deregistration =
+        HttpRequest.newBuilder(uri).header("Accept", "application/json").DELETE().build()
+    val response = HttpClient.newHttpClient().send(deregistration, BodyHandlers.ofString())
+    if (response.statusCode() != 204) {
+      val error =
+          String.format(
+              "Could not unregister source, expected 204, got: %s\n%s",
+              response.statusCode(),
+              response.body(),
+          )
+      throw RuntimeException(error)
+    }
+  }
+
+  private fun registrationJson(): String {
+    return java.lang
+        .StringBuilder()
+        .append("{")
+        .append("\"name\": ")
+        .append(quoted(name))
+        .append(",")
+        .append("\"config\": {")
+        .append("\"topic\": ")
+        .append(quoted(topic))
+        .append(",")
+        .append("\"connector.class\": \"streams.kafka.connect.source.Neo4jSourceConnector\",")
+        .append("\"key.converter\": \"io.confluent.connect.avro.AvroConverter\",")
+        .append("\"key.converter.schema.registry.url\": ")
+        .append(quoted(schemaControlRegistry))
+        .append(",")
+        .append("\"value.converter\": \"io.confluent.connect.avro.AvroConverter\",")
+        .append("\"value.converter.schema.registry.url\": ")
+        .append(quoted(schemaControlRegistry))
+        .append(",")
+        .append("\"neo4j.server.uri\": ")
+        .append(quoted(neo4jServerUri))
+        .append(",")
+        .append("\"neo4j.authentication.basic.username\": ")
+        .append(quoted(neo4jBasicUsername))
+        .append(",")
+        .append("\"neo4j.authentication.basic.password\": ")
+        .append(quoted(neo4jBasicPassword))
+        .append(",")
+        .append("\"neo4j.streaming.poll.interval.msecs\": ")
+        .append(pollInterval.toMillis())
+        .append(",")
+        .append("\"neo4j.streaming.property\": ")
+        .append(quoted(streamingNeo4jProperty))
+        .append(",")
+        .append("\"neo4j.streaming.from\": ")
+        .append(quoted(streamingStart.name))
+        .append(",")
+        .append("\"neo4j.enforce.schema\": ")
+        .append(enforceSchema)
+        .append(",")
+        .append("\"neo4j.source.query\": ")
+        .append(quoted(streamingQuery))
+        .append("}")
+        .append("}")
+        .toString()
+  }
+
+  companion object {
+    private fun quoted(string: String): String {
+      return String.format("\"%s\"", string)
+    }
+
+    private fun randomized(baseName: String): String {
+      val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+      val suffix =
+          ThreadLocalRandom.current()
+              .ints(8, 0, charPool.size)
+              .asSequence()
+              .map(charPool::get)
+              .joinToString("")
+      return "${baseName}_$suffix"
+    }
+  }
+}
