8a06dc88f9a6240ae19d372a2b1feb1af8d15fa2
Andrea Santurbano <santand@gmail.com>
2023-10-08 14:51:23 +0200

    fix: port fix for neo4j-contrib/neo4j-streams/issues/567
    Fix PointValue property writes to sink.
    
    See https://github.com/neo4j-contrib/neo4j-streams/commit/59bac5723d97c6d1b74fa161e490a43a015cb249

--- common/src/main/kotlin/org/neo4j/connectors/kafka/utils/JSONUtils.kt
+++ common/src/main/kotlin/org/neo4j/connectors/kafka/utils/JSONUtils.kt
@@ -18,8 +18,12 @@ package org.neo4j.connectors.kafka.utils
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
@@ -31,11 +35,19 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.IOException
 import java.time.temporal.TemporalAccessor
 import kotlin.reflect.full.isSubclassOf
+import org.neo4j.connectors.kafka.events.EntityType
+import org.neo4j.connectors.kafka.events.Meta
+import org.neo4j.connectors.kafka.events.NodePayload
+import org.neo4j.connectors.kafka.events.Payload
+import org.neo4j.connectors.kafka.events.RecordChange
+import org.neo4j.connectors.kafka.events.RelationshipPayload
+import org.neo4j.connectors.kafka.events.Schema
 import org.neo4j.connectors.kafka.events.StreamsTransactionEvent
 import org.neo4j.connectors.kafka.events.StreamsTransactionNodeEvent
 import org.neo4j.connectors.kafka.events.StreamsTransactionRelationshipEvent
 import org.neo4j.connectors.kafka.extensions.asStreamsMap
 import org.neo4j.driver.Value
+import org.neo4j.driver.Values
 import org.neo4j.driver.internal.value.PointValue
 import org.neo4j.driver.types.Node
 import org.neo4j.driver.types.Point
@@ -121,6 +133,140 @@ class DriverRelationshipSerializer : JsonSerializer<Relationship>() {
   }
 }
 
+class StreamsTransactionRelationshipEventDeserializer :
+    StreamsTransactionEventDeserializer<
+        StreamsTransactionRelationshipEvent, RelationshipPayload>() {
+  override fun createEvent(
+      meta: Meta,
+      payload: RelationshipPayload,
+      schema: Schema
+  ): StreamsTransactionRelationshipEvent {
+    return StreamsTransactionRelationshipEvent(meta, payload, schema)
+  }
+
+  override fun convertPayload(payloadMap: JsonNode): RelationshipPayload {
+    return JSONUtils.convertValue<RelationshipPayload>(payloadMap)
+  }
+
+  override fun fillPayload(
+      payload: RelationshipPayload,
+      beforeProps: Map<String, Any>?,
+      afterProps: Map<String, Any>?
+  ): RelationshipPayload {
+    return payload.copy(
+        before = payload.before?.copy(properties = beforeProps),
+        after = payload.after?.copy(properties = afterProps))
+  }
+
+  override fun deserialize(
+      parser: JsonParser,
+      context: DeserializationContext
+  ): StreamsTransactionRelationshipEvent {
+    val deserialized = super.deserialize(parser, context)
+    if (deserialized.payload.type == EntityType.node) {
+      throw IllegalArgumentException("Relationship event expected, but node type found")
+    }
+    return deserialized
+  }
+}
+
+class StreamsTransactionNodeEventDeserializer :
+    StreamsTransactionEventDeserializer<StreamsTransactionNodeEvent, NodePayload>() {
+  override fun createEvent(
+      meta: Meta,
+      payload: NodePayload,
+      schema: Schema
+  ): StreamsTransactionNodeEvent {
+    return StreamsTransactionNodeEvent(meta, payload, schema)
+  }
+
+  override fun convertPayload(payloadMap: JsonNode): NodePayload {
+    return JSONUtils.convertValue<NodePayload>(payloadMap)
+  }
+
+  override fun fillPayload(
+      payload: NodePayload,
+      beforeProps: Map<String, Any>?,
+      afterProps: Map<String, Any>?
+  ): NodePayload {
+    return payload.copy(
+        before = payload.before?.copy(properties = beforeProps),
+        after = payload.after?.copy(properties = afterProps))
+  }
+
+  override fun deserialize(
+      parser: JsonParser,
+      context: DeserializationContext
+  ): StreamsTransactionNodeEvent {
+    val deserialized = super.deserialize(parser, context)
+    if (deserialized.payload.type == EntityType.relationship) {
+      throw IllegalArgumentException("Node event expected, but relationship type found")
+    }
+    return deserialized
+  }
+}
+
+abstract class StreamsTransactionEventDeserializer<EVENT, PAYLOAD : Payload> :
+    JsonDeserializer<EVENT>() {
+
+  abstract fun createEvent(meta: Meta, payload: PAYLOAD, schema: Schema): EVENT
+
+  abstract fun convertPayload(payloadMap: JsonNode): PAYLOAD
+
+  abstract fun fillPayload(
+      payload: PAYLOAD,
+      beforeProps: Map<String, Any>?,
+      afterProps: Map<String, Any>?
+  ): PAYLOAD
+
+  @Throws(IOException::class, JsonProcessingException::class)
+  override fun deserialize(parser: JsonParser, context: DeserializationContext): EVENT {
+    val root: JsonNode = parser.codec.readTree(parser)
+    val meta = JSONUtils.convertValue<Meta>(root["meta"])
+    val schema = JSONUtils.convertValue<Schema>(root["schema"])
+    val points = schema.properties.filterValues { it == "PointValue" }.keys
+    var payload = convertPayload(root["payload"])
+    if (points.isNotEmpty()) {
+      val beforeProps = convertPoints(payload.before, points)
+      val afterProps = convertPoints(payload.after, points)
+      payload = fillPayload(payload, beforeProps, afterProps)
+    }
+    return createEvent(meta, payload, schema)
+  }
+
+  private fun convertPoints(recordChange: RecordChange?, points: Set<String>) =
+      recordChange?.properties?.mapValues {
+        if (points.contains(it.key)) {
+          val pointMap = it.value as Map<String, Any>
+          when (pointMap["crs"]) {
+            "cartesian" ->
+                Values.point(
+                    7203, pointMap["x"].toString().toDouble(), pointMap["y"].toString().toDouble())
+            "cartesian-3d" ->
+                Values.point(
+                    9157,
+                    pointMap["x"].toString().toDouble(),
+                    pointMap["y"].toString().toDouble(),
+                    pointMap["z"].toString().toDouble())
+            "wgs-84" ->
+                Values.point(
+                    4326,
+                    pointMap["longitude"].toString().toDouble(),
+                    pointMap["latitude"].toString().toDouble())
+            "wgs-84-3d" ->
+                Values.point(
+                    4979,
+                    pointMap["longitude"].toString().toDouble(),
+                    pointMap["latitude"].toString().toDouble(),
+                    pointMap["height"].toString().toDouble())
+            else -> throw IllegalArgumentException("CRS value: ${pointMap["crs"]} not found")
+          }
+        } else {
+          it.value
+        }
+      }
+}
+
 object JSONUtils {
 
   private val OBJECT_MAPPER: ObjectMapper = jacksonObjectMapper()
@@ -133,10 +279,16 @@ object JSONUtils {
     module.addSerializer(PointValue::class.java, PointValueSerializer())
     module.addSerializer(Node::class.java, DriverNodeSerializer())
     module.addSerializer(Relationship::class.java, DriverRelationshipSerializer())
+    module.addDeserializer(
+        StreamsTransactionRelationshipEvent::class.java,
+        StreamsTransactionRelationshipEventDeserializer())
+    module.addDeserializer(
+        StreamsTransactionNodeEvent::class.java, StreamsTransactionNodeEventDeserializer())
     module.addSerializer(TemporalAccessor::class.java, TemporalAccessorSerializer())
     OBJECT_MAPPER.registerModule(module)
     OBJECT_MAPPER.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
     OBJECT_MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    STRICT_OBJECT_MAPPER.registerModule(module)
   }
 
   fun getObjectMapper(): ObjectMapper = OBJECT_MAPPER
@@ -173,6 +325,7 @@ object JSONUtils {
         val strValue =
             when (value) {
               is ByteArray -> String(value)
+              null -> ""
               else -> value.toString()
             }
         strValue.trimStart().let { if (it[0] == '{' || it[0] == '[') throw e else it as T }
