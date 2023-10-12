7a2c81d47f415260ff06114901e9b6ed531479ce
Florent Biville <florent.biville@neo4j.com>
2023-10-12 18:05:11 +0200

    ci: bump poll duration

--- source/src/test/kotlin/org/neo4j/connectors/kafka/source/testing/TopicVerifier.kt
+++ source/src/test/kotlin/org/neo4j/connectors/kafka/source/testing/TopicVerifier.kt
@@ -49,7 +49,7 @@ class TopicVerifier<K, V>(private val consumer: KafkaConsumer<K, V>) {
     val receivedMessages = RingBuffer<V>(predicates.size)
     try {
       await().atMost(timeout).until {
-        consumer.poll(Duration.ofMillis(500)).forEach { receivedMessages.add(it.value()) }
+        consumer.poll(Duration.ofMillis(2000)).forEach { receivedMessages.add(it.value()) }
         val messages = receivedMessages.toList()
         messages.size == predicates.size &&
             predicates.foldIndexed(true) { i, prev, predicate ->
