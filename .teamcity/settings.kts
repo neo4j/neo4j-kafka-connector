a2375147d2ff39113856c5e14cb27235e41d7c2e
Ali Ince <ali.ince@neo4j.com>
2023-10-19 11:17:09 +0100

    ci: configure vcs branch spec explicitly

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import builds.Build
 import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.version
 
 version = "2023.05"
@@ -11,6 +12,14 @@ project {
     text("github-packages-user", "neo4j-build-service")
     password("github-packages-token", "credentialsJSON:d5ea2df2-7a81-41d4-98bf-cb7ebd607493")
   }
+
+  vcsRoot(
+      GitVcsRoot {
+        name = "git@github.com:neo4j/neo4j-kafka-connector.git"
+        url = "git@github.com:neo4j/neo4j-kafka-connector.git"
+        branch = "refs/heads/main"
+        branchSpec = "refs/heads/*"
+      })
 
   subProject(
       Build(
