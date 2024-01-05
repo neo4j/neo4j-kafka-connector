9209cc4c1815935380492bf7f159e3bf946c1f1e
Ali Ince <ali.ince@neo4j.com>
2024-01-05 10:11:28 +0000

    ci: remove trailing whitespace

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -20,14 +20,14 @@ project {
           name = "main",
           branchFilter =
               """
-                +:main               
-            """
+                +:main
+              """
                   .trimIndent(),
           triggerRules =
               """
                 -:comment=^build.*release version.*:**
                 -:comment=^build.*update version.*:**
-            """
+              """
                   .trimIndent(),
           forPullRequests = false))
   subProject(
