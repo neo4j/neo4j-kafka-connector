48cecc0a9c106219810cef2faf192dc1a8f028fa
Ali Ince <ali.ince@neo4j.com>
2024-02-01 15:53:42 +0000

    test: generate sequential changes to avoid same timestamps

--- source/src/test/kotlin/org/neo4j/connectors/kafka/source/Neo4jQueryTaskTest.kt
+++ source/src/test/kotlin/org/neo4j/connectors/kafka/source/Neo4jQueryTaskTest.kt
@@ -444,6 +444,7 @@ class Neo4jQueryTaskTest {
       longToInt: Boolean = false
   ): List<Map<String, Any>> =
       session.beginTransaction().use { tx ->
+        val ts = clock.millis()
         val elements =
             (1..totalRecords).map {
               val result =
@@ -469,7 +470,7 @@ class Neo4jQueryTaskTest {
                                 |   n AS node
                             """
                           .trimMargin(),
-                      mapOf("timestamp" to clock.millis()))
+                      mapOf("timestamp" to ts + it))
               val next = result.next()
               val map = next.asMap().toMutableMap()
               map["array"] =
