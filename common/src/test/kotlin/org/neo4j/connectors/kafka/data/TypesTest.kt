4d700dedc2b5626e41f2bc2a8fe8624a0e45312e
Ali Ince <ali.ince@neo4j.com>
2024-02-09 10:32:41 +0000

    feat: add support for constructing change events

--- common/src/test/kotlin/org/neo4j/connectors/kafka/data/TypesTest.kt
+++ common/src/test/kotlin/org/neo4j/connectors/kafka/data/TypesTest.kt
@@ -30,12 +30,14 @@ import org.apache.kafka.connect.data.SchemaBuilder
 import org.apache.kafka.connect.data.Struct
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Named
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.neo4j.cdc.client.CDCClient
+import org.neo4j.connectors.kafka.data.ChangeEventExtensions.toChangeEvent
 import org.neo4j.connectors.kafka.data.ChangeEventExtensions.toConnectValue
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
@@ -53,7 +55,6 @@ class TypesTest {
     val neo4j: Neo4jContainer<*> =
         Neo4jContainer("neo4j:5-enterprise")
             .withEnv("NEO4J_ACCEPT_LICENSE_AGREEMENT", "yes")
-            .withNeo4jConfig("internal.dbms.change_data_capture", "true")
             .withoutAuthentication()
 
     private lateinit var driver: Driver
@@ -74,20 +75,27 @@ class TypesTest {
     }
   }
 
+  @BeforeEach
+  fun `start with an empty database`() {
+    driver.session().use { it.run("CREATE OR REPLACE DATABASE neo4j WAIT").consume() }
+  }
+
   @ParameterizedTest(name = "{0}")
   @MethodSource("org.neo4j.connectors.kafka.data.TypesTest\$SimpleDriverValues#values")
-  fun `should build schema and values out of simple driver values`(
+  fun `should build schema and values out of simple driver values and convert them back`(
       input: Any,
       expectedSchema: Schema,
       expectedValue: Any
   ) {
     driver.session().use {
       val returned = it.run("RETURN \$value", mapOf("value" to input)).single().get(0).asObject()
-      val schema = DynamicTypes.schemaFor(returned)
-      val converted = DynamicTypes.valueFor(schema, returned)
+      val schema = DynamicTypes.toConnectSchema(returned)
+      val converted = DynamicTypes.toConnectValue(schema, returned)
+      val reverted = DynamicTypes.fromConnectValue(schema, converted)
 
       schema shouldBe expectedSchema
       converted shouldBe expectedValue
+      reverted shouldBe input
     }
   }
 
@@ -115,7 +123,7 @@ class TypesTest {
               .single()
 
       val person = record.get("p").asNode()
-      schemaAndValue(person).also { (schema, value) ->
+      schemaAndValue(person).also { (schema, converted, reverted) ->
         schema shouldBe
             SchemaBuilder.struct()
                 .name("org.neo4j.connectors.kafka.Node")
@@ -126,17 +134,25 @@ class TypesTest {
                 .field("dob", SimpleTypes.LOCALDATE.schema())
                 .build()
 
-        value shouldBe
+        converted shouldBe
             Struct(schema)
                 .put("<id>", person.id())
                 .put("<labels>", person.labels().toList())
                 .put("name", "john")
                 .put("surname", "doe")
                 .put("dob", "1999-12-31")
+
+        reverted shouldBe
+            mapOf(
+                "<id>" to person.id(),
+                "<labels>" to person.labels().toList(),
+                "name" to "john",
+                "surname" to "doe",
+                "dob" to LocalDate.of(1999, 12, 31))
       }
 
       val company = record.get("c").asNode()
-      schemaAndValue(company).also { (schema, value) ->
+      schemaAndValue(company).also { (schema, converted, reverted) ->
         schema shouldBe
             SchemaBuilder.struct()
                 .name("org.neo4j.connectors.kafka.Node")
@@ -146,16 +162,23 @@ class TypesTest {
                 .field("est", SimpleTypes.LOCALDATE.schema())
                 .build()
 
-        value shouldBe
+        converted shouldBe
             Struct(schema)
                 .put("<id>", company.id())
                 .put("<labels>", company.labels().toList())
                 .put("name", "acme corp")
                 .put("est", "1980-01-01")
+
+        reverted shouldBe
+            mapOf(
+                "<id>" to company.id(),
+                "<labels>" to company.labels().toList(),
+                "name" to "acme corp",
+                "est" to LocalDate.of(1980, 1, 1))
       }
 
       val worksFor = record.get("r").asRelationship()
-      schemaAndValue(worksFor).also { (schema, value) ->
+      schemaAndValue(worksFor).also { (schema, converted, reverted) ->
         schema shouldBe
             SchemaBuilder.struct()
                 .name("org.neo4j.connectors.kafka.Relationship")
@@ -167,7 +190,7 @@ class TypesTest {
                 .field("since", SimpleTypes.LOCALDATE.schema())
                 .build()
 
-        value shouldBe
+        converted shouldBe
             Struct(schema)
                 .put("<id>", worksFor.id())
                 .put("<type>", worksFor.type())
@@ -175,15 +198,24 @@ class TypesTest {
                 .put("<end.id>", worksFor.endNodeId())
                 .put("contractId", 5916L)
                 .put("since", "2000-01-05")
+
+        reverted shouldBe
+            mapOf(
+                "<id>" to worksFor.id(),
+                "<type>" to worksFor.type(),
+                "<start.id>" to worksFor.startNodeId(),
+                "<end.id>" to worksFor.endNodeId(),
+                "contractId" to 5916L,
+                "since" to LocalDate.of(2000, 1, 5))
       }
     }
   }
 
   @Test
-  fun `should build schema and value for change events`() {
+  fun `should build schema and value for change events and convert back`() {
     // set-up cdc
     driver.session().use {
-      it.run("CREATE OR REPLACE DATABASE neo4j OPTIONS { txLogEnrichment: 'FULL' } WAIT").consume()
+      it.run("ALTER DATABASE neo4j SET OPTION txLogEnrichment 'FULL' WAIT").consume()
 
       // set-up constraints
       it.run("CREATE CONSTRAINT FOR (n:Person) REQUIRE (n.name, n.surname) IS KEY").consume()
@@ -226,6 +258,9 @@ class TypesTest {
               eventSchema.name() shouldBe "org.neo4j.connectors.kafka.cdc.NodeEvent"
             }
         value.get("event") should beInstanceOf<Struct>()
+
+        val reverted = value.toChangeEvent()
+        reverted shouldBe change
       }
 
       changes[2].also { change ->
@@ -241,6 +276,9 @@ class TypesTest {
               eventSchema.name() shouldBe "org.neo4j.connectors.kafka.cdc.RelationshipEvent"
             }
         value.get("event") should beInstanceOf<Struct>()
+
+        val reverted = value.toChangeEvent()
+        reverted shouldBe change
       }
     }
   }
@@ -280,7 +318,11 @@ class TypesTest {
           Arguments.of(
               Named.of("duration", Values.isoDuration(5, 2, 23, 5).asIsoDuration()),
               SimpleTypes.DURATION.schema(),
-              "P5M2DT23.000000005S"),
+              Struct(SimpleTypes.DURATION.schema())
+                  .put("months", 5L)
+                  .put("days", 2L)
+                  .put("seconds", 23L)
+                  .put("nanoseconds", 5)),
           Arguments.of(
               Named.of("point - 2d", Values.point(7203, 2.3, 4.5).asPoint()),
               SimpleTypes.POINT.schema(),
@@ -298,7 +340,7 @@ class TypesTest {
                   .put("y", 56.7)
                   .put("z", 100.0)),
           Arguments.of(
-              Named.of("list - uniformly typed elements", (1..50)),
+              Named.of("list - uniformly typed elements", (1L..50L).toList()),
               SchemaBuilder.array(SimpleTypes.LONG.schema()).build(),
               (1L..50L).toList()),
           Arguments.of(
@@ -344,9 +386,10 @@ class TypesTest {
     }
   }
 
-  private fun schemaAndValue(value: Any): Pair<Schema, Any?> {
-    val schema = DynamicTypes.schemaFor(value)
-    val converted = DynamicTypes.valueFor(schema, value)
-    return Pair(schema, converted)
+  private fun schemaAndValue(value: Any): Triple<Schema, Any?, Any?> {
+    val schema = DynamicTypes.toConnectSchema(value)
+    val converted = DynamicTypes.toConnectValue(schema, value)
+    val reverted = DynamicTypes.fromConnectValue(schema, converted)
+    return Triple(schema, converted, reverted)
   }
 }
