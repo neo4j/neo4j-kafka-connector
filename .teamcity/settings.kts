2d5343aefa6eca11b4610cd4d7f543674e661ac6
Ali Ince <ali.ince@neo4j.com>
2024-04-18 14:49:56 +0100

    ci: update version

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,7 +3,7 @@ import builds.Neo4jKafkaConnectorVcs
 import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.version
 
-version = "2023.11"
+version = "2024.03"
 
 project {
   params {
