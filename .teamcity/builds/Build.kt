51449f07c91ae00b9394c49414a6001c9923baa1
emrehizal <emre.hizal@neo4j.com>
2024-09-11 11:25:07 +0200

    ci: improve pipeline logic

--- .teamcity/builds/Build.kt
+++ .teamcity/builds/Build.kt
@@ -1,11 +1,17 @@
 package builds
 
 import jetbrains.buildServer.configs.kotlin.Project
-import jetbrains.buildServer.configs.kotlin.StageFactory.parallel
-import jetbrains.buildServer.configs.kotlin.StageFactory.sequential
 import jetbrains.buildServer.configs.kotlin.sequential
 import jetbrains.buildServer.configs.kotlin.toId
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+enum class JavaPlatform(
+    val javaVersion: String = DEFAULT_JAVA_VERSION,
+    val platformVersions: List<String> = listOf(DEFAULT_CONFLUENT_PLATFORM_VERSION)
+) {
+  JDK_11("11", platformVersions = listOf("7.2.9", "7.7.0")),
+  JDK_17("17", platformVersions = listOf("7.7.0"))
+}
 
 class Build(
     name: String,
@@ -13,105 +19,80 @@ class Build(
     forPullRequests: Boolean,
     triggerRules: String? = null
 ) :
-    Project({
-      this.id(name.toId())
-      this.name = name
+    Project(
+        {
+          this.id(name.toId())
+          this.name = name
 
-      val complete = Empty("${name}-complete", "complete")
+          val complete = Empty("${name}-complete", "complete")
 
-      val bts = sequential {
-        if (forPullRequests)
-            buildType(WhiteListCheck("${name}-whitelist-check", "white-list check"))
-        if (forPullRequests) dependentBuildType(PRCheck("${name}-pr-check", "pr check"))
-
-        val packaging11 =
-            Maven("${name}-package", "package", "package", "11", "-pl :packaging -am -DskipTests")
-
-        val packaging17 =
-            Maven("${name}-package", "package", "package", "17", "-pl :packaging -am -DskipTests")
-
-        parallel {
-          // JDK 11
-          sequential {
-            dependentBuildType(Maven("${name}-build", "build", "test-compile", "11"))
-            dependentBuildType(Maven("${name}-unit-tests", "unit tests", "test", "11"))
-            dependentBuildType(collectArtifacts(packaging11))
+          val bts = sequential {
+            if (forPullRequests)
+                buildType(WhiteListCheck("${name}-whitelist-check", "white-list check"))
+            if (forPullRequests) dependentBuildType(PRCheck("${name}-pr-check", "pr check"))
 
             parallel {
-              dependentBuildType(
-                  IntegrationTests(
-                      "${name}-integration-tests", "integration tests", "11", "7.2.9") {
-                        dependencies {
-                          artifacts(packaging11) {
-                            artifactRules =
-                                """
-                        +:packages/*.jar => docker/plugins
-                        -:packages/*-kc-oss.jar
-                      """
-                                    .trimIndent()
+              JavaPlatform.entries.forEach { p ->
+                val packaging =
+                    Maven(
+                        "${name}-package",
+                        "package",
+                        "package",
+                        p.javaVersion,
+                        "-pl :packaging -am -DskipTests")
+
+                dependentBuildType(Maven("${name}-build", "build", "test-compile", "11"))
+                dependentBuildType(Maven("${name}-unit-tests", "unit tests", "test", "11"))
+                dependentBuildType(collectArtifacts(packaging))
+
+                parallel {
+                  p.platformVersions.forEach { version ->
+                    dependentBuildType(
+                        IntegrationTests(
+                            "${name}-integration-tests",
+                            "integration tests",
+                            p.javaVersion,
+                            version,
+                        ) {
+                          dependencies {
+                            artifacts(packaging) {
+                              artifactRules =
+                                  """
+                      +:packages/*.jar => docker/plugins
+                      -:packages/*-kc-oss.jar
+                    """
+                                      .trimIndent()
+                            }
                           }
-                        }
-                      })
-              dependentBuildType(
-                  IntegrationTests(
-                      "${name}-integration-tests", "integration tests", "11", "7.7.0") {
-                        dependencies {
-                          artifacts(packaging11) {
-                            artifactRules =
-                                """
-                        +:packages/*.jar => docker/plugins
-                        -:packages/*-kc-oss.jar
-                      """
-                                    .trimIndent()
-                          }
-                        }
-                      })
+                        },
+                    )
+                  }
+                }
+              }
+            }
+
+            dependentBuildType(complete)
+            if (!forPullRequests)
+                dependentBuildType(Release("${name}-release", "release", DEFAULT_JAVA_VERSION))
+          }
+
+          bts.buildTypes().forEach {
+            it.thisVcs()
+
+            it.features {
+              requireDiskSpace("5gb")
+              enableCommitStatusPublisher()
+              if (forPullRequests) enablePullRequests()
+            }
+
+            buildType(it)
+          }
+
+          complete.triggers {
+            vcs {
+              this.branchFilter = branchFilter
+              this.triggerRules = triggerRules
             }
           }
-
-          // JDK 17
-          sequential {
-            dependentBuildType(Maven("${name}-build", "build", "test-compile", "17"))
-            dependentBuildType(Maven("${name}-unit-tests", "unit tests", "test", "17"))
-            dependentBuildType(collectArtifacts(packaging17))
-
-            dependentBuildType(
-                IntegrationTests("${name}-integration-tests", "integration tests", "17", "7.7.0") {
-                  dependencies {
-                    artifacts(packaging17) {
-                      artifactRules =
-                          """
-                        +:packages/*.jar => docker/plugins
-                        -:packages/*-kc-oss.jar
-                      """
-                              .trimIndent()
-                    }
-                  }
-                })
-          }
-        }
-
-        dependentBuildType(complete)
-        if (!forPullRequests)
-            dependentBuildType(Release("${name}-release", "release", DEFAULT_JAVA_VERSION))
-      }
-
-      bts.buildTypes().forEach {
-        it.thisVcs()
-
-        it.features {
-          requireDiskSpace("5gb")
-          enableCommitStatusPublisher()
-          if (forPullRequests) enablePullRequests()
-        }
-
-        buildType(it)
-      }
-
-      complete.triggers {
-        vcs {
-          this.branchFilter = branchFilter
-          this.triggerRules = triggerRules
-        }
-      }
-    })
+        },
+    )
