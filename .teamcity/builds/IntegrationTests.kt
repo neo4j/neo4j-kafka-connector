7155d9b08ef2dcdbc2179d7f594a85ae00e8036b
Ali Ince <ali.ince@neo4j.com>
2024-11-20 23:06:44 +0000

    ci: collect files into a zip

--- .teamcity/builds/IntegrationTests.kt
+++ .teamcity/builds/IntegrationTests.kt
@@ -25,7 +25,7 @@ class IntegrationTests(
 
           artifactRules =
               """
-              +:diagnostics => diagnostics
+              +:diagnostics => diagnostics.zip
               """
                   .trimIndent()
 
