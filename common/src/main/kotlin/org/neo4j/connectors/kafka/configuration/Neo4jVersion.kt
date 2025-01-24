d4d5eea28db71af8b37cb9b041a94c6a79b3654c
emrehizal <emre.hizal@neo4j.com>
2025-01-24 15:41:51 +0100

    fix: parse neo4j versions with new scheme

--- common/src/main/kotlin/org/neo4j/connectors/kafka/configuration/Neo4jVersion.kt
+++ common/src/main/kotlin/org/neo4j/connectors/kafka/configuration/Neo4jVersion.kt
@@ -1,11 +1,28 @@
-package org.neo4j.connectors.kafka
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.connectors.kafka.configuration
 
+import java.util.*
 import kotlin.math.sign
 
-class Neo4jVersion @JvmOverloads constructor(
-  private val major: Int,
-  private val minor: Int,
-  private val patch: Int = Int.MAX_VALUE
+class Neo4jVersion(
+    private val major: Int,
+    private val minor: Int,
+    private val patch: Int = Int.MAX_VALUE
 ) : Comparable<Neo4jVersion> {
 
   override fun compareTo(other: Neo4jVersion): Int {
@@ -18,9 +35,27 @@ class Neo4jVersion @JvmOverloads constructor(
     return signum(patch - other.patch)
   }
 
+  override fun equals(other: Any?): Boolean {
+    if (other !is Neo4jVersion) {
+      return false
+    }
+    return major == other.major && minor == other.minor && patch == other.patch
+  }
+
+  override fun hashCode(): Int {
+    return Objects.hash(major, minor, patch)
+  }
+
+  override fun toString(): String {
+    if (patch == Int.MAX_VALUE) {
+      return String.format("%d.%d", major, minor)
+    }
+    return String.format("%d.%d.%d", major, minor, patch)
+  }
+
   companion object {
-    private val v4_4 = Neo4jVersion(4, 4, 0)
-    private val v5 = Neo4jVersion(5, 0, 0)
+    val v5 = Neo4jVersion(5, 0, 0)
+    val v4_4 = Neo4jVersion(4, 4, 0)
 
     fun of(version: String): Neo4jVersion {
       var major = -1
@@ -36,13 +71,21 @@ class Neo4jVersion @JvmOverloads constructor(
           major = buffer.toInt(10)
         } else if (minor == -1) {
           minor = parseMinor(buffer)
+        } else {
+          throw invalidVersion(version)
         }
         buffer = ""
       }
       if (buffer.isNotEmpty()) {
-        patch = buffer.toInt(10)
+        if (minor == -1) {
+          minor = parseMinor(buffer)
+        } else {
+          patch = buffer.toInt(10)
+        }
       }
-      require(!(major == -1 || minor == -1)) { String.format("Invalid Neo4j version: %s", version) }
+      if (major == -1 || minor == -1) {
+        throw invalidVersion(version)
+      }
       if (patch == -1) {
         return Neo4jVersion(major, minor)
       }
@@ -55,6 +98,10 @@ class Neo4jVersion @JvmOverloads constructor(
 
     private fun signum(result: Int): Int {
       return sign(result.toDouble()).toInt()
+    }
+
+    private fun invalidVersion(version: String): IllegalArgumentException {
+      return IllegalArgumentException(String.format("Invalid Neo4j version: %s", version))
     }
   }
 }
--- common/src/test/kotlin/org/neo4j/connectors/kafka/configuration/Neo4jVersionTest.kt
+++ common/src/test/kotlin/org/neo4j/connectors/kafka/configuration/Neo4jVersionTest.kt
@@ -1,4 +1,44 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.neo4j.connectors.kafka.configuration
 
-import org.junit.jupiter.api.Assertions.*
- class Neo4jVersionTest
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class Neo4jVersionTest {
+
+  @Test
+  fun `should parse version string`() {
+    assertEquals(Neo4jVersion.of("4.4"), Neo4jVersion(4, 4))
+    assertEquals(Neo4jVersion.of("4.4-aura"), Neo4jVersion(4, 4))
+    assertEquals(Neo4jVersion.of("4.4.13"), Neo4jVersion(4, 4, 13))
+    assertEquals(Neo4jVersion.of("2025.01"), Neo4jVersion(2025, 1))
+    assertEquals(Neo4jVersion.of("2025.01.0"), Neo4jVersion(2025, 1, 0))
+    assertEquals(Neo4jVersion.of("2025.01-aura"), Neo4jVersion(2025, 1))
+  }
+
+  @Test
+  fun `should fail invalid version format`() {
+    assertThrows<IllegalArgumentException> { Neo4jVersion.of("") }
+    assertThrows<IllegalArgumentException> { Neo4jVersion.of("5") }
+    assertThrows<IllegalArgumentException> { Neo4jVersion.of("5.5.3.1") }
+    assertThrows<NumberFormatException> { Neo4jVersion.of("..") }
+    assertThrows<NumberFormatException> { Neo4jVersion.of("5..3") }
+    assertThrows<NumberFormatException> { Neo4jVersion.of(".2025.6") }
+  }
+}
