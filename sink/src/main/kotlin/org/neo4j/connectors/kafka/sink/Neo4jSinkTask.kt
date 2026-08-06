8c62402227991ffe92b7cb39a94c1eae617f4f0a
vasif-abdulhamidov <vasif.abdulhamidov@neo4j.com>
2026-08-06 16:16:32 +0100

    build: security findings to apply group by topic and partition

--- sink/src/main/kotlin/org/neo4j/connectors/kafka/sink/Neo4jSinkTask.kt
+++ sink/src/main/kotlin/org/neo4j/connectors/kafka/sink/Neo4jSinkTask.kt
@@ -69,9 +69,10 @@ class Neo4jSinkTask(private val metricsFactory: MetricsFactory = MetricsFactory(
     val duration = measureTime {
       records
           ?.map { SinkMessage(it, config) }
-          ?.groupBy { it.topic }
-          ?.mapKeys { topicHandlers.getValue(it.key) }
-          ?.forEach { (handler, messages) -> processMessages(handler, messages) }
+          ?.groupBy { it.topic to it.record.kafkaPartition() }
+          ?.forEach { (topicPartition, messages) ->
+            processMessages(topicHandlers.getValue(topicPartition.first), messages)
+          }
     }
     log.info("processed {} records in {} ms", records?.size ?: 0, duration.inWholeMilliseconds)
   }
