bebdc1db2b034a1c8b937ca34f425b6facd6612b
Eugene Rubanov <eugene.rubanov@neo4j.com>
2026-08-14 15:46:16 +0100

    fix: add setting to docker compose, remove volumes, add IF NOT exist to database removal query
    (cherry picked from commit e947dd0b4140abd1a30fe07c840a0cc889a43619)

--- scripts/tear-down-test-environment.sh
+++ scripts/tear-down-test-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
 # This file exists to prevent issues with creating a script dynamically, which can occur is the build agent is running low on disk.
-dip compose down --rmi local
+dip compose down --rmi local --volumes
