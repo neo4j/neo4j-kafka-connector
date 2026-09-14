2a5c59bfe3f0b8eb5c89bef4e71d367861825cba
Erik Nord <erik.nord@neo4j.com>
2026-09-14 13:16:08 +0200

    test: add unit and IT for UUID string convert
    in SinkBatchStrategy for unit test of the function
    
    in {Apoc,Native}BatchStrategy for integration with caniuse preidactes

--- sink/src/test/kotlin/org/neo4j/connectors/kafka/sink/strategy/ApocBatchStrategyTest.kt
+++ sink/src/test/kotlin/org/neo4j/connectors/kafka/sink/strategy/ApocBatchStrategyTest.kt
@@ -19,7 +19,9 @@ package org.neo4j.connectors.kafka.sink.strategy
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.collections.shouldMatchInOrder
 import io.kotest.matchers.shouldBe
+import java.util.UUID
 import java.util.stream.Stream
+import org.apache.kafka.connect.sink.SinkRecord
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
@@ -31,6 +33,7 @@ import org.neo4j.caniuse.Neo4j
 import org.neo4j.caniuse.Neo4jDeploymentType
 import org.neo4j.caniuse.Neo4jEdition
 import org.neo4j.caniuse.Neo4jVersion
+import org.neo4j.connectors.kafka.sink.SinkMessage
 import org.neo4j.connectors.kafka.sink.SinkStrategy
 import org.neo4j.connectors.kafka.sink.strategy.cdc.CdcSchemaEventTransformer
 
@@ -127,6 +130,8 @@ class ApocBatchStrategyTest {
           Arguments.of(neo4j5_27_aura, callSubqueryWithVariableScope()),
           Arguments.of(neo4j2026_1, callSubqueryWithVariableScope()),
           Arguments.of(neo4j2026_1_aura, callSubqueryWithVariableScope()),
+          Arguments.of(neo4j2026_8, callSubqueryWithVariableScope()),
+          Arguments.of(neo4j2026_8_aura, callSubqueryWithVariableScope()),
       )
     }
   }
@@ -174,6 +179,58 @@ class ApocBatchStrategyTest {
     )
   }
 
+  @ParameterizedTest
+  @ArgumentsSource(UuidTypeProvider::class)
+  fun `should cast UUId as string for unsupported neo4j`(
+      neo4j: Neo4j,
+      shouldCastToString: Boolean,
+  ) {
+    val uuid = UUID.randomUUID()
+    val strategy = ApocBatchStrategy(neo4j, 2, "", SinkStrategy.CDC_SCHEMA)
+    val message = SinkMessage(SinkRecord("my-topic", 0, null, null, null, null, 0))
+
+    val action =
+        CreateNodeSinkAction(
+            setOf("Entity"),
+            mapOf("id" to uuid, "ids" to listOf(uuid), "id-like" to uuid.toString(), "sum" to 3),
+        )
+
+    val query = strategy.handle(listOf(message)) { action }.single().single().query
+    val properties = query.parameters()["events"][0]["params"]["e"]["properties"]
+
+    val expectedUuid: Any =
+        if (shouldCastToString) {
+          uuid.toString()
+        } else {
+          uuid
+        }
+
+    properties.asMap() shouldBe
+        mapOf(
+            "id" to expectedUuid,
+            "ids" to listOf(expectedUuid),
+            "id-like" to uuid.toString(), // always a string
+            "sum" to 3,
+        )
+  }
+
+  object UuidTypeProvider : ArgumentsProvider {
+    override fun provideArguments(
+        parameters: ParameterDeclarations,
+        context: ExtensionContext,
+    ): Stream<out Arguments> =
+        Stream.of(
+            Arguments.of(neo4j4_4, true),
+            Arguments.of(neo4j5_18, true),
+            Arguments.of(neo4j5_26, true),
+            Arguments.of(neo4j5_27_aura, true),
+            Arguments.of(neo4j2026_1, true),
+            Arguments.of(neo4j2026_1_aura, true),
+            Arguments.of(neo4j2026_8, false),
+            Arguments.of(neo4j2026_8_aura, false),
+        )
+  }
+
   companion object {
     private val neo4j4_4 =
         Neo4j(Neo4jVersion(4, 4), Neo4jEdition.ENTERPRISE, Neo4jDeploymentType.SELF_MANAGED)
@@ -187,5 +244,9 @@ class ApocBatchStrategyTest {
         Neo4j(Neo4jVersion(2026, 1), Neo4jEdition.ENTERPRISE, Neo4jDeploymentType.SELF_MANAGED)
     private val neo4j2026_1_aura =
         Neo4j(Neo4jVersion(2026, 1), Neo4jEdition.ENTERPRISE, Neo4jDeploymentType.AURA)
+    private val neo4j2026_8 =
+        Neo4j(Neo4jVersion(2026, 8), Neo4jEdition.ENTERPRISE, Neo4jDeploymentType.SELF_MANAGED)
+    private val neo4j2026_8_aura =
+        Neo4j(Neo4jVersion(2026, 8), Neo4jEdition.ENTERPRISE, Neo4jDeploymentType.AURA)
   }
 }
