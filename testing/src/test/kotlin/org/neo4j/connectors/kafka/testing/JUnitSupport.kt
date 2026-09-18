871a1894d8066b171b6e33edbd2bb49af9fb2119
Eugene Rubanov <eugene.rubanov@neo4j.com>
2026-09-18 17:09:13 +0100

    test: update extension tests

--- testing/src/test/kotlin/org/neo4j/connectors/kafka/testing/JUnitSupport.kt
+++ testing/src/test/kotlin/org/neo4j/connectors/kafka/testing/JUnitSupport.kt
@@ -54,11 +54,10 @@ internal object JUnitSupport {
 
     return mock<ExtensionContext> {
       on { testClass } doReturn Optional.ofNullable(method.javaMethod!!.declaringClass)
-      on { requiredTestClass } doReturn method.javaMethod!!.declaringClass
       on { testMethod } doReturn Optional.ofNullable(method.javaMethod)
-      on { requiredTestMethod } doReturn method.javaMethod!!
       on { displayName } doReturn method.name
       on { getStore(any()) } doReturn store
+      on { uniqueId } doReturn UUID.randomUUID().toString()
     }
   }
 }
